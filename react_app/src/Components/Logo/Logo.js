e3b70eedcbee1db0e52d2b9e594feae6efc98825
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-03-16 20:26:08 +0100

    Finish styling logo component
    style sing, container and image

--- react_app/src/Components/Logo/Logo.js
+++ react_app/src/Components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './Logo.module.scss';
-const Logo = (props) => {
+const Logo = () => {
    return (
       <div className={styles.container}>
          <img src={require('./email.svg')} alt='Email logo'/>
+         <p className={styles.sign} >My mail</p>
       </div>
    );
 };
