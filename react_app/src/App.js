213289bc91ddaf45c80721a9f1961b996879d8d9
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-03-28 20:30:38 +0100

    Add first menu draft
    add sizes of menu, change vh to vw in width of all components

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import styles from './App.module.scss';
 import Header from './Components/Header/Header';
+import Menu from './Components/Menu/Menu';
 
 class App extends Component {
    render() {
       return (
          <div className={styles.App}>
             <Header/>
+            <Menu/>
          </div>
       );
    }
---
+++ react_app/src/Components/Menu/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Menu.module.scss';
+
+const Menu = (props) => {
+   return (
+      <div className={styles.container}>
+
+      </div>
+   );
+};
+export default Menu;
