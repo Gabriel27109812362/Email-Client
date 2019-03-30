934a9d6aa1889c6fefae0d05f3ee4b5648dd7aeb
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-03-30 19:04:31 +0100

    Start making options button

---
+++ src/Components/Menu/Option/Option.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Option.module.scss';
+
+const Option = (props) => {
+   return (
+      <button className={styles.option}>
+         option
+      </button>
+   );
+};
+export default Option;
