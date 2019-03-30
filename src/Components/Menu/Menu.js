fe5abecc41c2da634dffc4e3a82f430d8db263fa
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-03-30 19:08:39 +0100

    Add new option button to menu
    button unstyled, starting work on option button

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Menu.module.scss';
 import NewMessage from './NewMessage/NewMessage';
+import Option from './Option/Option';
+
 
 const Menu = (props) => {
    return (
       <div className={styles.container}>
       <NewMessage/>
+      <Option/>
       </div>
    );
 };
